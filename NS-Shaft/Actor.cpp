b5b03a905fd91fd6e38caf00984bdb83f0ca8b5d
sou <soueleven@gmail.com>
2020-06-01 23:42:02 +0900

    Add actor and component

---
+++ NS-Shaft/Actor.cpp
@@ -0,0 +1,84 @@
+#include "Actor.h"
+#include "Component.h"
+#include "Game.h"
+
+Actor::Actor(Game* game) 
+	:mState(EActive),
+	mPosition(0.0f, 0.0f),
+	mGame(game)
+{
+	mGame->AddActor(this);
+}
+
+Actor::~Actor()
+{
+	mGame->RemoveActor(this);
+	// Need to delete components
+	// Because ~Component calls RemoveComponent, need a different style loop
+	while (!mComponents.empty())
+	{
+		delete mComponents.back();
+	}
+}
+
+void Actor::Update(float deltaTime)
+{
+	if (mState == EActive)
+	{
+		UpdateComponents(deltaTime);
+		UpdateActor(deltaTime);
+	}
+}
+
+void Actor::UpdateComponents(float deltaTime)
+{
+	for (auto comp : mComponents)
+	{
+		comp->Update(deltaTime);
+	}
+}
+
+void Actor::UpdateActor(float deltaTime)
+{
+}
+
+void Actor::ProcessInput(const uint8_t* keyState)
+{
+	if (mState == EActive)
+	{
+		// First process input for components
+		for (auto comp : mComponents)
+		{
+			comp->ProcessInput(keyState);
+		}
+	}
+}
+
+void Actor::AddComponent(Component* component)
+{
+	// Find the insertion point in the sorted vector
+	// (The first element with a order higher than me)
+	int myOrder = component->GetUpdateOrder();
+	auto iter = mComponents.begin();
+	for (;
+		iter != mComponents.end();
+		++iter)
+	{
+		if (myOrder < (*iter)->GetUpdateOrder())
+		{
+			break;
+		}
+	}
+
+	// Inserts element before position of iterator
+	mComponents.insert(iter, component);
+}
+
+void Actor::RemoveComponent(Component* component)
+{
+	auto iter = std::find(mComponents.begin(), mComponents.end(), component);
+	if (iter != mComponents.end())
+	{
+		mComponents.erase(iter);
+	}
+}
---
+++ NS-Shaft/Actor.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include <cstdint>
+#include "Vector2.h"
+
+class Actor
+{
+public:
+	enum State
+	{
+		EActive,
+		EPaused,
+		EDead
+	};
+
+	Actor(class Game* game);
+	virtual ~Actor();
+
+	// Update function called from Game (not overridable)
+	void Update(float deltaTime);
+	// Updates all the components attached to the actor (not overridable)
+	void UpdateComponents(float deltaTime);
+	// Any actor-specific update code (overridable)
+	virtual void UpdateActor(float deltaTime);
+
+	// ProcessInput function called from Game (not overridable)
+	void ProcessInput(const uint8_t* keyState);
+
+	// Getters/setters
+	const Vector2& GetPosition() const { return mPosition; }
+	void SetPosition(const Vector2& pos) { mPosition = pos; }
+
+	State GetState() const { return mState; }
+	void SetState(State state) { mState = state; }
+
+	class Game* GetGame() { return mGame; }
+
+
+	// Add/remove components
+	void AddComponent(class Component* component);
+	void RemoveComponent(class Component* component);
+private:
+	// Actor's state
+	State mState;
+
+	// Transform
+	Vector2 mPosition;
+
+	std::vector<class Component*> mComponents;
+	class Game* mGame;
+};
