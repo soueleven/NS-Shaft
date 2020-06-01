04d8a5e9c74b15a9eebdfacdd8d480d86e6f3df0
sou <soueleven@gmail.com>
2020-06-01 23:42:27 +0900

    Add vector class

---
+++ NS-Shaft/Vector2.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// 2D Vector
+class Vector2
+{
+public:
+	float x;
+	float y;
+
+	Vector2()
+		:x(0.0f)
+		, y(0.0f)
+	{}
+
+	explicit Vector2(float inX, float inY)
+		:x(inX)
+		, y(inY)
+	{}
+};
